069f832af85c3eb1a60265ad539c70c04341f2bd
Jieyi Guo <e0533401@u.nus.edu>
2020-08-17 18:22:24 +0800

    //fix login

--- app/src/main/java/iss/workshop/myapplication/MainActivity.java
+++ app/src/main/java/iss/workshop/myapplication/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     EditText inputUsername;
     EditText inputPassword;
@@ -24,9 +24,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         inputUsername = (EditText) findViewById(R.id.inputUsername);
         inputPassword = (EditText) findViewById(R.id.inputPassword);
         Login = (Button)findViewById(R.id.loginBtn);
-        if (Login != null) {
-            Login.setOnClickListener(this);
-        }
 
         SharedPreferences pref = getSharedPreferences("user_account",MODE_PRIVATE);
         if(pref.contains("username") && pref.contains("password")){
@@ -35,34 +32,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startHomeActivity();
             }
         }
+
+        Login.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                String username = inputUsername.getText().toString();
+                String password = inputPassword.getText().toString();
+
+                boolean loginConfirm = checkAccount(username,password);
+                if (loginConfirm) {
+                    SharedPreferences pref = getSharedPreferences("user_account", MODE_PRIVATE);
+                    SharedPreferences.Editor editor = pref.edit();
+                    editor.putString("username", username);
+                    editor.putString("password", password);
+                    editor.commit();
+
+                    Toast.makeText(getApplicationContext(), "Login successful!", Toast.LENGTH_SHORT).show();
+                    startHomeActivity();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Username/Password are invalid", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
-    @Override
-    public void onClick(View v) {
-        int id = v.getId();
-        if (id == R.id.loginBtn) {
-            String username = inputPassword.getText().toString();
-            String password = inputPassword.getText().toString();
-
-            boolean loginConfirm = checkAccount(username,password);
-            if (loginConfirm) {
-                SharedPreferences pref = getSharedPreferences("user_account", MODE_PRIVATE);
-                SharedPreferences.Editor editor = pref.edit();
-                editor.putString("username", username);
-                editor.putString("password", password);
-                editor.commit();
-
-                Toast.makeText(getApplicationContext(), "Login successful!", Toast.LENGTH_SHORT).show();
-                startHomeActivity();
-            }
-            else {
-                Toast.makeText(getApplicationContext(), "Username/Password are invalid", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
 
     private boolean checkAccount(String username, String password){
-        if (username.equals("123") && password.equals("123")){
+        if (username.equals("John") && password.equals("password")){
             return true;
         }
         return false;
