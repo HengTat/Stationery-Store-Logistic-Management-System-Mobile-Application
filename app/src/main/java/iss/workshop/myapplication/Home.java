d9c1a5443ec265f63b56b5ff4f78b82eec0e3973
Jieyi Guo <e0533401@u.nus.edu>
2020-08-17 18:36:28 +0800

    //fix logout

--- app/src/main/java/iss/workshop/myapplication/Home.java
+++ app/src/main/java/iss/workshop/myapplication/Home.java
@@ -1,14 +1,18 @@
 package iss.workshop.myapplication;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Home extends AppCompatActivity implements View.OnClickListener {
+public class Home extends AppCompatActivity {
 
     Button Logout;
+    TextView user;
+    SharedPreferences pref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,16 +20,22 @@ public class Home extends AppCompatActivity implements View.OnClickListener {
         setContentView(R.layout.activity_home);
 
         Logout = (Button)findViewById(R.id.logoutBtn);
-        if(Logout!=null){
-            Logout.setOnClickListener(this);
-        }
+        user = (TextView)findViewById(R.id.username);
+
+        pref = (SharedPreferences)getSharedPreferences("user_account",MODE_PRIVATE);
+        String username = pref.getString("username", "");
+        user.setText("Welcome " + username);
+
+        Logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = pref.edit();
+                editor.clear();
+                editor.commit();
+
+                finish();
+            }
+        });
     }
 
-    @Override
-    public void onClick(View v){
-        int id = v.getId();
-        if(id == R.id.logoutBtn){
-            finish();
-        }
-    }
 }
